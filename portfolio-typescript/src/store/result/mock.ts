20c39ca461c1482e593c9e969877332f7897d769
gwaksuyeon <kwak7663@gmail.com>
2020-03-20 18:26:25 +0900

    survey, result mock data 생성

---
+++ portfolio-typescript/src/store/result/mock.ts
@@ -0,0 +1,241 @@
+
+export const Data = [
+  {
+    id: 1,
+    manufacturer: '오뚜기', // 제조사
+    name: '진라면 순한맛', // 이름
+    evaluation: 5, // 평가
+    img: '', // img
+  },
+  {
+    id: 2,
+    manufacturer: '팔도', // 제조사
+    name: '비빔면', // 이름
+    evaluation: 5, // 평가
+    img: '', // img
+  },
+  {
+    id: 3,
+    manufacturer: '동대문엽기떡볶이', // 제조사
+    name: '착한맛', // 이름
+    evaluation: 5, // 평가
+    img: '', // img
+  },
+  {
+    id: 4,
+    manufacturer: '푸라닭', // 제조사
+    name: '고추마요', // 이름
+    evaluation: 5, // 평가
+    img: '', // img
+  },
+  {
+    id: 5,
+    manufacturer: '굽네치킨', // 제조사
+    name: '고추바사삭', // 이름
+    evaluation: 5, // 평가
+    img: '', // img
+  },
+  {
+    id: 6,
+    manufacturer: '팔도', // 제조사
+    name: '남자라면', // 이름
+    evaluation: 4, // 평가
+    img: '', // img
+  },
+  {
+    id: 7,
+    manufacturer: '팔도', // 제조사
+    name: '쫄비빔면', // 이름
+    evaluation: 4, // 평가
+    img: '', // img
+  },
+  {
+    id: 8,
+    manufacturer: '농심', // 제조사
+    name: '신라면', // 이름
+    evaluation: 4, // 평가
+    img: '', // img
+  },
+  {
+    id: 9,
+    manufacturer: '오뚜기', // 제조사
+    name: '진라면 매운맛', // 이름
+    evaluation: 4, // 평가
+    img: '', // img
+  },
+  {
+    id: 10,
+    manufacturer: '삼양', // 제조사
+    name: '까르보불닭볶음면', // 이름
+    evaluation: 4, // 평가
+    img: '', // img
+  },
+  {
+    id: 11,
+    manufacturer: '농심', // 제조사
+    name: '너구리 매운맛', // 이름
+    evaluation: 4, // 평가
+    img: '', // img
+  },
+  {
+    id: 12,
+    manufacturer: '오뚜기', // 제조사
+    name: '열라면', // 이름
+    evaluation: 4, // 평가
+    img: '', // img
+  },
+  {
+    id: 13,
+    manufacturer: '동대문엽기떡볶이', // 제조사
+    name: '초보맛', // 이름
+    evaluation: 4, // 평가
+    img: '', // img
+  },
+  {
+    id: 14,
+    manufacturer: '신전떡볶이', // 제조사
+    name: '순한맛', // 이름
+    evaluation: 4, // 평가
+    img: '', // img
+  },
+  {
+    id: 15,
+    manufacturer: '교촌치킨', // 제조사
+    name: '레드콤보', // 이름
+    evaluation: 4, // 평가
+    img: '', // img
+  },
+  {
+    id: 16,
+    manufacturer: '삼양', // 제조사
+    name: '불닭볶음면', // 이름
+    evaluation: 3, // 평가
+    img: '', // img
+  },
+  {
+    id: 17,
+    manufacturer: '삼양', // 제조사
+    name: '커리불닭볶음면', // 이름
+    evaluation: 3, // 평가
+    img: '', // img
+  },
+  {
+    id: 18,
+    manufacturer: '팔도', // 제조사
+    name: '괄도네넴띤', // 이름
+    evaluation: 3, // 평가
+    img: '', // img
+  },
+  {
+    id: 19,
+    manufacturer: '농심', // 제조사
+    name: '앵그리 너구리', // 이름
+    evaluation: 3, // 평가
+    img: '', // img
+  },
+  {
+    id: 20,
+    manufacturer: '동대문엽기떡볶이', // 제조사
+    name: '덜매운맛', // 이름
+    evaluation: 3, // 평가
+    img: '', // img
+  },
+  {
+    id: 21,
+    manufacturer: '굽네치킨', // 제조사
+    name: '볼케이노', // 이름
+    evaluation: 3, // 평가
+    img: '', // img
+  },
+  {
+    id: 22,
+    manufacturer: '농심', // 제조사
+    name: '짜왕 매운맛', // 이름
+    evaluation: 2, // 평가
+    img: '', // img
+  },
+  {
+    id: 23,
+    manufacturer: '오뚜기', // 제조사
+    name: '진짜쫄면', // 이름
+    evaluation: 2, // 평가
+    img: '', // img
+  },
+  {
+    id: 24,
+    manufacturer: '삼양', // 제조사
+    name: '마라불닭볶음면', // 이름
+    evaluation: 2, // 평가
+    img: '', // img
+  },
+  {
+    id: 25,
+    manufacturer: '동대문엽기떡볶이', // 제조사
+    name: '오리지널', // 이름
+    evaluation: 2, // 평가
+    img: '', // img
+  },
+  {
+    id: 26,
+    manufacturer: '신전떡볶이', // 제조사
+    name: '보통맛', // 이름
+    evaluation: 2, // 평가
+    img: '', // img
+  },
+  {
+    id: 27,
+    manufacturer: '후라이드참잘하는집', // 제조사
+    name: '디진다치킨', // 이름
+    evaluation: 2, // 평가
+    img: '', // img
+  },
+  {
+    id: 28,
+    manufacturer: '60계치킨', // 제조사
+    name: '6초치킨', // 이름
+    evaluation: 2, // 평가
+    img: '', // img
+  },
+  {
+    id: 29,
+    manufacturer: '팔도', // 제조사
+    name: '틈새라면', // 이름
+    evaluation: 1, // 평가
+    img: '', // img
+  },
+  {
+    id: 30,
+    manufacturer: '삼양', // 제조사
+    name: '핵불닭볶음면', // 이름
+    evaluation: 1, // 평가
+    img: '', // img
+  },
+  {
+    id: 31,
+    manufacturer: '동대문엽기떡볶이', // 제조사
+    name: '매운맛', // 이름
+    evaluation: 1, // 평가
+    img: '', // img
+  },
+  {
+    id: 32,
+    manufacturer: '신전떡볶이', // 제조사
+    name: '매운맛', // 이름
+    evaluation: 1, // 평가
+    img: '', // img
+  },
+  {
+    id: 33,
+    manufacturer: 'BBQ', // 제조사
+    name: '뱀파이어치킨(3단계)', // 이름
+    evaluation: 1, // 평가
+    img: '', // img
+  },
+  {
+    id: 34,
+    manufacturer: '네네치킨', // 제조사
+    name: '쇼킹핫치킨', // 이름
+    evaluation: 1, // 평가
+    img: '', // img
+  },
+]
---
+++ portfolio-typescript/src/store/survey/mock.ts
@@ -0,0 +1,63 @@
+
+export const Data = [
+  {
+    id: 1,
+    manufacturer: '팔도', // 제조사
+    name: '비빔면', // 이름
+    img: '', // img
+  },
+  {
+    id: 2,
+    manufacturer: '동대문엽기떡볶이', // 제조사
+    name: '착한맛', // 이름
+    img: '', // img
+  },
+  {
+    id: 3,
+    manufacturer: '농심', // 제조사
+    name: '신라면', // 이름
+    img: '', // img
+  },
+  {
+    id: 4,
+    manufacturer: '교촌치킨', // 제조사
+    name: '레드콤보', // 이름
+    img: '', // img
+  },
+  {
+    id: 5,
+    manufacturer: '삼양', // 제조사
+    name: '불닭볶음면', // 이름
+    img: '', // img
+  },
+  {
+    id: 6,
+    manufacturer: '동대문엽기떡볶이', // 제조사
+    name: '덜매운맛', // 이름
+    img: '', // img
+  },
+  {
+    id: 7,
+    manufacturer: '동대문엽기떡볶이', // 제조사
+    name: '오리지널', // 이름
+    img: '', // img
+  },
+  {
+    id: 8,
+    manufacturer: '신전떡볶이', // 제조사
+    name: '보통맛', // 이름
+    img: '', // img
+  },
+  {
+    id: 9,
+    manufacturer: '삼양', // 제조사
+    name: '핵불닭볶음면', // 이름
+    img: '', // img
+  },
+  {
+    id: 10,
+    manufacturer: '네네치킨', // 제조사
+    name: '쇼킹핫', // 이름
+    img: '', // img
+  },
+]
