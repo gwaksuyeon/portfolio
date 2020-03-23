808174b5d1b814be0930a5b6e1cdf8eab60be031
gwaksuyeon <kwak7663@gmail.com>
2020-03-23 16:44:23 +0900

    icon 생성 및 수정

---
+++ portfolio-typescript/src/component/icons/check.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Check = () => {
+  return (
+    <svg x="0px" y="0px" viewBox="0 0 192 192">
+      <g>
+        <path d="M165.2,30l-89.6,89.4L26.8,70.8L5,92.6L75.6,163L187,51.8L165.2,30z"/>
+      </g>
+    </svg>
+  )
+}
+
+export default Check;
