74647bff99736e64568685ffdab5be438b47d291
gwaksuyeon <kwak7663@gmail.com>
2020-03-24 17:28:08 +0900

    basename 변경

--- portfolio-typescript/src/Root.tsx
+++ portfolio-typescript/src/Root.tsx
@@ -4,7 +4,7 @@ import App from './App';
 
 const Root = () => {
   return (
-    <BrowserRouter basename="/planet">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App/>
     </BrowserRouter>
   )
