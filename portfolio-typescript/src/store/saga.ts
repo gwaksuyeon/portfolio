3b12887d7ad3223efe7ef66cea1122b5cf05c8fa
gwaksuyeon <kwak7663@gmail.com>
2020-03-24 16:43:43 +0900

    result redux

--- portfolio-typescript/src/store/saga.ts
+++ portfolio-typescript/src/store/saga.ts
@@ -1,7 +1,9 @@
 import { fork } from 'redux-saga/effects';
 
 import { Saga as SurveySaga } from 'store/survey';
+import { Saga as ResultSaga } from 'store/result';
 
 export function* RootSaga() {
   yield fork(SurveySaga);
+  yield fork(ResultSaga);
 }
