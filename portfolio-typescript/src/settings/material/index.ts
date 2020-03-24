12a2c910ca0b0b868fc8f5c5d043ad09e7541248
gwaksuyeon <kwak7663@gmail.com>
2020-03-24 21:38:53 +0900

    로그인, 회원가입 password 안보이는 문제 해결 (font-family)

--- portfolio-typescript/src/settings/material/index.ts
+++ portfolio-typescript/src/settings/material/index.ts
@@ -4,12 +4,16 @@ export const Theme = createMuiTheme({
   palette: {
     primary: {
       main: '#DB2017'
+    },
+    text: {
+      primary: '#505050'
     }
   },
   typography: {
+    fontSize: 12,
     fontFamily: [
       'NanumSquare',
       'sans-serif',
     ].join(','),
-  }
+  },
 })
